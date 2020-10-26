1a4f2b7275f380711632a72b0d87076851c7ee3f
ibrahimizh <ibrahim_na@outlook.com>
2020-10-26 17:54:26 -0300

    Rendered Sorted List of Customers from APICall

--- src/CustomersList.js
+++ src/CustomersList.js
@@ -2,6 +2,8 @@ import React,{useState,useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import APICall from './util';
 
+import './CustomersList.css';
+
 export default () => {
     const [myCustomers,setMyCustomers] = useState([]);
 
@@ -14,10 +16,37 @@ export default () => {
         fetchMyCustomers();
     },[]);
 
+    console.log("myCustomers");
     console.log(myCustomers);
 
+    const sortedCustomers = myCustomers.sort((a,b)=>a.business_name > b.business_name ? 1:-1);
+
+    console.log("sortedCustomers");
+    console.log(sortedCustomers);
+
+    const renderedCustomers=Object.values(
+        sortedCustomers
+        .map((customer,index) => {
+        return (
+            <div key={index} className="row">
+                <label className="businessName">
+                    {customer.business_name}
+                </label>
+                <label className="address">
+                    {customer.city}, {customer.province}
+                </label>
+            </div>
+        );
+    }));
+
     return (
-        <h1>My Customers</h1>
+        <div className="roundedPanel">
+            <div className="row heading">
+                <h4>My Customers</h4>
+            </div>
+            {renderedCustomers}            
+            <div className="footer">&nbsp;</div>
+        </div>
     );
 };
 
